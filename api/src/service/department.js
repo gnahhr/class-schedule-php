4901d95e77288d692fcd60914d98e89a1e710865
xscold <vashivan116@gmail.com>
2024-08-28 21:50:32 +0800

    added include model course department

--- api/src/service/department.js
+++ api/src/service/department.js
@@ -1,9 +1,14 @@
 const DEPARTMENT = require('../models/department');
 const ERROR_MESSAGE = require('../constants/error-message');
 const createService = require('./template');
+const Course = require('../models/course');
 
 const fields = ['name', 'code'];
 
-const service = createService(DEPARTMENT, ERROR_MESSAGE, fields);
+const include = [
+  { model: Course},
+];
+
+const service = createService(DEPARTMENT, ERROR_MESSAGE, fields, include);
 
 module.exports = service;
